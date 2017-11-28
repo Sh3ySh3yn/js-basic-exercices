1a6bd1e31e34521c37a3975a40b696b714025932
Scharazade Compper <scharazade.compper@gmail.com>
2017-11-28 14:08:55 +0100

    exercice modifié

--- exercices_js_1.js
+++ exercices_js_1.js
@@ -7,31 +7,59 @@
 
 function hello() {
 	// write your code HERE
+  console.log("Hello World!");
 };
 
 // ----
 // écrire des fonctions suivante:
 // addition(a, b)
+function addition(a,b) {
+  return a + b;
+};
 // soustraction(a, b)
+function soustraction(a,b) {
+  return a - b;
+};
 // multiplication(a, b)
+function multiplication(a,b) {
+  return a * b;
+};
 // division(a, b)
+function division(a,b) {
+  return a / b;
+};
 // concatenation(str1, str2)
+function concatenation(str1, str2) {
+  console.log(str1 + str2);
+};
 // moyenne(a1, a2, a3)
-
-// Chaque fonction devra effectuer l'opération correspondant à son nom, et renvoyer le résultat. 
+function moyenne(a1, a2, a3) {
+  return (a1 + a2 + a3) / 3;
+};
+// Chaque fonction devra effectuer l'opération correspondant à son nom, et renvoyer le résultat.
 // Celui-ci sera stocké dans une variable, qui sera ensuite affichée dans la console
- 
+
 // ----
 // Déclarer deux variables a et b, de valeurs respectives 5 et 12
+var a = 5;
+var b = 12;
 // A l'aide d'une variable intermédiaire, inverser les deux valeurs pour que a ait pour valeur 12 et b pour valeur 5
 // Afficher le résultat de a et b dans la console sous la forme d'une phrase: "A vaut : xxx et B vaut yyy"
-var a, b;
+var c = b;
+b = a;
+a = c;
+console.log("A vaut : "+ a + " et B vaut : "+ b);
 // WRITE YOUR CODE HERE
 
 // ----
 // Ecrire une fonction 'isDivisibleBy5' qui reçoit un nombre en paramètre, et qui vérifie que ce nombre est divisible par 5.
 // Cette fonction doit renvoyer un booléen
 var isDivisibleBy5 = function(x) {
+  if (x % 5 == 0) {
+    return "true";
+  } else {
+    return "false";
+  }
 	// WRITE YOUR CODE HERE
 };
 
@@ -39,16 +67,20 @@ var isDivisibleBy5 = function(x) {
 // Ecrire une fonction 'isDivisibleByN' qui reçoit deux nombres entiers x et n, et qui vérifie si x est divisible par n.
 // Cette fonction doit renvoyer un booléen
 function isDivisibleByN(x, n) {
+  console.log( x % n == 0);
 	// WRITE YOUR CODE HERE
-}
+};
 
 // ----
 // Générer un nombre décimal compris entre 0 et 1, et le stocker dans une variable
 // Changer la valeur de cette variable pour qu'elle vaille le nombre initial arrondi à 2 décimales
 // Afficher dans la console
-function genereEtArrondit() {
+var d = 0.5654;
+function genereEtArrondit(d) {
   // WRITE YOUR CODE HERE
-}
+  d.tofixed(2);
+  return d.tofixed(2);
+};
 
 
 /**
@@ -56,19 +88,19 @@ function genereEtArrondit() {
 **/
 
 // ----
-// Ecrire une fonction qui reçoit une chaîne de caractères, et qui renvoie la position 
+// Ecrire une fonction qui reçoit une chaîne de caractères, et qui renvoie la position
 // de la première occurence de la lettre 'e' dans cette chaîne
 
 // ----
-// Ecrire une fonction qui reçoit une chaîne de caractères, et qui renvoie la position 
+// Ecrire une fonction qui reçoit une chaîne de caractères, et qui renvoie la position
 // de la dernière occurence de la lettre 'e' dans cette chaîne
 
 // ----
-// Ecrire une fonction qui reçoit une chaîne de caractères contenant une prhase entière, 
+// Ecrire une fonction qui reçoit une chaîne de caractères contenant une prhase entière,
 // et qui renvoie un tableau contenant chacun des différents mots dans la phrase
 
 // ----
-// Ecrire une fonction qui reçoit une chaîne de caractère, qui transforme 
+// Ecrire une fonction qui reçoit une chaîne de caractère, qui transforme
 // la première lettre en majuscule, et qui renvoie la nouvelle chaîne
 
 
@@ -99,7 +131,7 @@ function genereEtArrondit() {
 
 // ----
 // Ecrire une fonction qui prend en argument un tableau de nombres et en calcule la somme.
-// Valeurs initiales: 
+// Valeurs initiales:
 var numTab = [45, 99, 357, -33, 1, 8, -78];
 function computeSumOfArray(tab) {
 	// WRITE YOUR CODE HERE
@@ -144,7 +176,7 @@ function computeSumOfArray(tab) {
 // - modifier dynamiquement le prénom de cet objet
 
 // ----
-// Ecrire une fonction qui reçoit un objet, et qui renvoie la longueur de cet objet 
+// Ecrire une fonction qui reçoit un objet, et qui renvoie la longueur de cet objet
 var getObjLength = function(obj) {
 	// WRITE YOUR CODE HERE
 };
